f4c40a9b477c78d500ce2ab79442df77af904fe2
Pepe del Moral <pepeleala@gmail.com>
2015-07-06 06:42:38 -0400

    isoHeroku

--- models/models.js
+++ models/models.js
@@ -1,13 +1,34 @@
 var path=require('path');
 
+// match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/)
+
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+
+var DB_name= (url[6]||null);
+var user= (url[2]||null);
+var pwd= (url[3]||null);
+var protocol= (url[1]||null);
+var dialect= (url[1]||null);
+var port= (url[5]||null);
+var host= (url[4]||null);
+var storage= process.env.DATABASE_STORAGE;
+
+
 //Cargar Modelo ORM
 var Sequelize =require('sequelize');
 
-//Usar BBDD SQLite:
-var sequelize= new Sequelize (null,null,null,
-	{dialect:"sqlite", storage:"consulta.sqlite"
+
+//Usar BBDD SQLite o Postgres
+
+var sequelize= new Sequelize (DB_name,user,pwd,
+	{dialect:protocol, 
+	 protocol:protocol,
+	 port:port,
+	 storage: storage, // solo SQLite (.env)
+	 omitNull:true // solo Postgres
 	}
 	);
+
 //Importa la definición de la tabla Isodata en consulta.js
 var Isodata=sequelize.import(path.join(__dirname,'consulta'));
 exports.Isodata=Isodata;//exporta la definición de la tabla Isodata
